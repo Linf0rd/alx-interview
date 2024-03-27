85f880155b5f1e69dd955b12caf473f9c6c2a31b
Linf0rd <linfordsnagger@gmx.com>
2024-03-27 15:49:01 +0200

    Implement pascal_triangle function for Pascal's triangle generation
    Defines a function pascal_triangle(n) that generates Pascal's triangle up to the nth row. Handles edge case (n <= 0) and iteratively builds the triangle using previous rows.

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A module for working with Pascal's triangle.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's Triangle up to the nth row.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
