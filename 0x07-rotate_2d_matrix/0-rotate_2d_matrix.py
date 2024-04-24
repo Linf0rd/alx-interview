7cf459a283f76f2aeb14f78c7c51d235f8ae2ead
Linf0rd <linfordsnagger@gmx.com>
2024-04-24 14:47:34 +0200

    Implement function to rotate 2D matrix clockwise in-place (alx-interview)
    * 0-rotate_2d_matrix.py: Defines function for in-place 2D matrix rotation (90 degrees clockwise).
    
    This commit introduces a Python function named rotate_2d_matrix within the 0-rotate_2d_matrix.py file. The function takes a 2D matrix as input and modifies it in-place to achieve a 90-degree clockwise rotation. It leverages a combination of transposing the matrix and reversing individual rows.

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,7 +8,7 @@ Args:
 
 
 def rotate_2d_matrix(matrix):
-  """
+    """
   Rotates a 2D matrix 90 degrees clockwise in-place.
 
   Performs the rotation by transposing the matrix and then reversing each row.
@@ -16,10 +16,10 @@ def rotate_2d_matrix(matrix):
   Args:
       matrix: A 2D list representing the n x n matrix.
   """
-  n = len(matrix)
-  for i in range(n):
-    for j in range(i, n):
-      matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-  for row in matrix:
-    row.reverse()
+    for row in matrix:
+        row.reverse()
